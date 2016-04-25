319301a3ffa2e870ba8a21da83d236de05bcbc69
GagnonStyle <spencer.gagnon95@gmail.com>
2016-04-24 23:47:52 -0400

    Adding new users works now, with a simple form

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,23 +3,47 @@ var model = require('../lib/users');
 var router = express.Router();
 
 router.get('/', (req,res) => {
+	var message = req.flash('users');
 	model.all(function(err, users){
 	      if(err){
 	        	console.log(err);
 	        	res.redirect('/');
 	      } else {
-		        res.render('all_users', { users: users });
+		        res.render('all_users', { users: users, message: message });
 	      }
     });
 });
 
 router.get('/new', (req,res) => {
-	res.render('new_user');
+	var message = req.flash('users');
+	res.render('new_user', { message: message });
 });
 
 
 router.post('/create', (req,res) => {
-	
+    var data = req.body;
+
+    (!data.is_admin) && (data.is_admin = false);
+    // if any empty inputs, redirect back to signup with message
+    if(!(data.username && data.town && data.password && data.confirm_password)){
+		req.flash('users', 'One or more required fields omitted');
+		res.redirect('/users/new'); 
+    } else if(data.password !== data.confirm_password) {
+		// check if passwords match
+		req.flash('users', 'Passwords must match');
+		res.redirect('/users/new');
+    } else {
+		// all good, add user
+		model.create(data, function(err, user) {
+			if(err) {
+			  req.flash('users', 'Error: ' + err);
+			  res.redirect('/users/new');
+			} else {
+			  req.flash('users', 'User created successfully!');
+			  res.redirect('/users');
+			}
+		});
+    }
 });
 
 
