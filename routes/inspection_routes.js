964971ba74db9443853a9626f104cd18d66ee2ab
Nick Sarnie <commendsarnex@gmail.com>
2016-04-25 15:19:45 -0400

    Inspections add to database
    Signed-off-by: Nick Sarnie <commendsarnex@gmail.com>

--- routes/inspection_routes.js
+++ routes/inspection_routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var model = require('../lib/inspections');
 var establishments = require('../lib/food_establishments');
-
 var router = express.Router();
 
 router.get('/', (req,res) => {
@@ -9,6 +8,36 @@ router.get('/', (req,res) => {
 });
 
 router.get('/new', (req,res) => {
+
+        var message = req.flash('inspections');
+	res.render('new_inspection', { message: message });
+        console.log(message);
+});
+
+router.post('/create', (req,res) => {
+    var data = req.body;
+
+    (!data.is_admin) && (data.is_admin = false);
+    // if any empty inputs, redirect back to signup with message
+
+    
+    
+    if(!(data.establishment_id && data.user_id && data.date_of_inspection && data.time_in && data.time_out && data.permit_number && data.type && data.action_required && data.risk_level && data.embargo && data.date_of_reinspection && data.signed)){
+		req.flash('inspections', 'One or more required fields omitted');
+		res.redirect('/inspections/new'); 
+    } else {
+		// all good, add user
+		model.create(data, function(err, user) {
+			if(err) {
+			  req.flash('inspections', 'Error: ' + err);
+			  res.redirect('/inspections/new');
+			} else {
+			  req.flash('inspections', 'Inspection created successfully!');
+			  res.redirect('/inspections');
+			}
+		});
+   }
+
 	establishments.all(function(err, establishments){
 		if(err){
 			req.flash('home', 'Error: ' + err);
@@ -17,6 +46,7 @@ router.get('/new', (req,res) => {
 			res.render('new_inspection', {food_establishments: establishments});			
 		}
 	});
+
 });
 
 
