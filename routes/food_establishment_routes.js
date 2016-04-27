b16800bde3c8305d32697b7586b727ac818003b9
GagnonStyle <spencer.gagnon95@gmail.com>
2016-04-26 20:40:07 -0400

    You can logout now, and it show when you're logged in or not

--- routes/food_establishment_routes.js
+++ routes/food_establishment_routes.js
@@ -3,42 +3,70 @@ var model = require('../lib/food_establishments');
 var router = express.Router();
 
 router.get('/', (req,res) => {
-	var message = req.flash('establishments');
-	model.all(function(err, establishments){
-	      if(err){
-	        	req.flash('home', err);
-	        	res.redirect('/');
-	      } else {
-		        res.render('all_establishments', { food_establishments: establishments, message: message });
-	      }
-    });
+	var user = req.session.user;
+
+	if(user){
+		var message = req.flash('establishments');
+		model.all(function(err, establishments){
+		      if(err){
+		        	req.flash('home', err);
+		        	res.redirect('/');
+		      } else {
+			        res.render('all_establishments', {
+			        	current_user: user, 
+			        	food_establishments: establishments, 
+			        	message: message 
+			        });
+		      }
+	    });
+	} else {
+		req.flash('login', 'You must be logged in to do this');
+		res.redirect('/users/login');
+	}
 });
 
 router.get('/new', (req,res) => {
-	var message = req.flash('establishments');
-	res.render('new_establishment', { message: message });
+	var user = req.session.user;
+
+	if(user){
+		var message = req.flash('establishments');
+		res.render('new_establishment', { 
+			current_user: user,
+			message: message 
+		});
+	} else {
+		req.flash('login', 'You must be logged in to do this');
+		res.redirect('/users/login');
+	}
 });
 
 
 router.post('/create', (req,res) => {
-    var data = req.body;
+	var user = req.session.user;
 
-    // if any empty inputs, redirect back to signup with message
-    if(!(data.name && data.town && data.address && data.person_in_charge && data.description)){
-		req.flash('establishments', 'One or more required fields omitted');
-		res.redirect('/establishments/new'); 
-    } else {
-		// all good, add establishment
-		model.create(data, function(err, establishment) {
-			if(err) {
-			  req.flash('establishments', 'Error: ' + err);
-			  res.redirect('/establishments/new');
-			} else {
-			  req.flash('establishments', 'New food establishment created successfully!');
-			  res.redirect('/establishments');
-			}
-		});
-    }
+	if(user){
+	    var data = req.body;
+
+	    // if any empty inputs, redirect back to new page 
+	    if(!(data.name && data.town && data.address && data.person_in_charge && data.description)){
+			req.flash('establishments', 'One or more required fields omitted');
+			res.redirect('/establishments/new'); 
+	    } else {
+			// all good, add establishment
+			model.create(data, function(err, establishment) {
+				if(err) {
+				  req.flash('establishments', 'Error: ' + err);
+				  res.redirect('/establishments/new');
+				} else {
+				  req.flash('establishments', 'New food establishment created successfully!');
+				  res.redirect('/establishments');
+				}
+			});
+	    }
+	} else {
+		req.flash('login', 'You must be logged in to do this');
+		res.redirect('/users/login');		
+	}
 });
 
 
--- routes/inspection_routes.js
+++ routes/inspection_routes.js
@@ -4,49 +4,72 @@ var establishments = require('../lib/food_establishments');
 var router = express.Router();
 
 router.get('/', (req,res) => {
-	res.render('all_inspections');
+	var user = req.session.user;
+	if(user){
+		res.render('all_inspections', { 
+			current_user: user 
+		});
+	} else {
+		req.flash('login', 'You must be logged in to do this');
+		res.redirect('/users/login');		
+	}
 });
 
 router.get('/new', (req,res) => {
-        var message = req.flash('inspections');
-        establishments.all(function(err, establishments){
-		if(err){
-			req.flash('home', 'Error: ' + err);
-			res.redirect('/')
-		} else {
-			res.render('new_inspection', {food_establishments: establishments, message:message});			
-		}
-	});
+	var user = req.session.user;
+	if(user){
+		var message = req.flash('inspections');
+	    establishments.all(function(err, establishments){
+			if(err){
+				req.flash('home', 'Error: ' + err);
+				res.redirect('/')
+			} else {
+				res.render('new_inspection', {
+					current_user: user, 
+					food_establishments: establishments, 
+					message: message
+				});			
+			}
+		});
+	} else {
+		req.flash('login', 'You must be logged in to do this');
+		res.redirect('/users/login');
+	}
 });
 
 router.post('/create', (req,res) => {
-    var data = req.body;
-    // if any empty inputs, redirect back to signup with message
-
-    //XXX: Let user input date
-    data.time_in = new Date();
-    data.time_out = new Date();
-
-    (!data.action_required) && (data.action_required = false);
-    (!data.embargo) && (data.embargo = false);
+	var user = req.session.user;
     
-    if(!(data.establishment_id && data.user_id && data.date_of_inspection && data.time_in && data.time_out && data.permit_number && data.type &&  data.risk_level &&  data.date_of_reinspection && data.signed)){
-		req.flash('inspections', 'One or more required fields omitted');
-		res.redirect('/inspections/new'); 
-    } else {
-		// all good, add user
-		model.create(data, function(err, user) {
-			if(err) {
-			  req.flash('inspections', 'Error: ' + err);
-			  res.redirect('/inspections/new');
-			} else {
-			  req.flash('inspections', 'Inspection created successfully!');
-			  res.redirect('/inspections');
-			}
-		});
-   }
+    if(user){
+	    var data = req.body;
+	    // if any empty inputs, redirect back to signup with message
 
-	
+	    //XXX: Let user input date
+	    data.time_in = new Date();
+	    data.time_out = new Date();
+
+	    (!data.action_required) && (data.action_required = false);
+	    (!data.embargo) && (data.embargo = false);
+	    
+	    if(!(data.establishment_id && data.user_id && data.date_of_inspection && data.time_in && data.time_out && data.permit_number && data.type &&  data.risk_level &&  data.date_of_reinspection && data.signed)){
+			req.flash('inspections', 'One or more required fields omitted');
+			res.redirect('/inspections/new'); 
+	    } else {
+			// all good, add user
+			model.create(data, function(err, user) {
+				if(err) {
+				  req.flash('inspections', 'Error: ' + err);
+				  res.redirect('/inspections/new');
+				} else {
+				  req.flash('inspections', 'Inspection created successfully!');
+				  res.redirect('/inspections');
+				}
+			});
+	    }
+	} else {
+		req.flash('login', 'You must be logged in to do this');
+		res.redirect('/users/login');
+	}
 
 });
 
